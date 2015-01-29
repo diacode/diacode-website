f76d3e9991b22d843556e50199ec9f8b01d1598a
Ricardo <bigardone@gmail.com>
2015-01-29 17:54:43 +0100

    Blog configuration

--- config.rb
+++ config.rb
@@ -69,6 +69,37 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  blog.taglink = "tags/:tag.html"
+  blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "/blog/tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page '/blog/*', layout: 'blog'
+
+page "/feed.xml", layout: false
+
 # Build-specific configuration
 configure :build do
   ignore 'images/*.psd'
